7085aa8bb729bd34e63f250d36308b7393994a13
AetherFabian <fabian_galarza@hotmail.com>
2022-05-17 21:11:19 -0600

    feat: add a post detail html and url

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
+from django import views
 from django.urls import path
-from .views import render_posts
+from .views import render_posts, post_detail
 
 urlpatterns = [
     path('', render_posts, name='posts'),
+    path("post/<int:post_id>", post_detail)
 ]
